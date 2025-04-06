9a5e3c3e632fea47237702a08a307f3e49524df0
SoftSisPro <softsispro@gmail.com>
2025-04-05 22:51:15 -0500

    filament base test function success

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/* Option livewire */
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post(env('LIVEWIRE_UPDATE_PATH'), $handle);
+});
+
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get(env('LIVEWIRE_JAVASCRIPT_PATH'), $handle);
 });
